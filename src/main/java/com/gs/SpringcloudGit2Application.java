0a1416fab207334552a36322e97fbb3d0b0f3a98
账号gs-cj <2469095052@qq.com>
2020-05-20 00:16:55 +0800

    初始化

---
+++ src/main/java/com/gs/SpringcloudGit2Application.java
@@ -0,0 +1,13 @@
+package com.gs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGit2Application.class, args);
+    }
+
+}
